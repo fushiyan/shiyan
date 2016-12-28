94e555b54901779df75ed4054c896124c6f901ec
shiyan <fushiyan@gmail.com>
2016-12-27 19:05:41 -0500

    Add project files.

---
+++ shiyan/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="shiyan.MvcApplication" Language="C#" %>
---
+++ shiyan/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(shiyan.Startup))]
+namespace shiyan
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
